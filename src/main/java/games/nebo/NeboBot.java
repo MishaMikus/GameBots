835e22aac68ac3c057b0c15644d22683c9508e90
Mykhaylo_Mikus <olga.lavinska@thomsonreuters.com>
2016-03-10 11:09:21 +0200

    split property

--- src/main/java/games/nebo/NeboBot.java
+++ src/main/java/games/nebo/NeboBot.java
@@ -3,11 +3,14 @@ package games.nebo;
 import framework.Property;
 import framework.model.User;
 
+import java.util.Arrays;
+
 public class NeboBot {
     public static void main(String[] arg) throws Exception {
         String url;
         String user;
         String pass;
+        System.out.println("START [===============================] "+Arrays.toString(arg));
         if (arg.length==3){
             url=arg[0];
             user=arg[1];
